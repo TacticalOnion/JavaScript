be95c23697ff495a327e1fe8895cce5ac55f16a3
Pablo Rojas <123708990+TacticalOnion@users.noreply.github.com>
2025-07-12 21:49:09 +0000

    feat(Practicas): agrega script para probar condicionales

---
+++ Practicas/08-condicionales.js
@@ -0,0 +1,39 @@
+/*if*/
+if(true){
+    /*Codigo*/
+}
+
+/*if-else*/
+if(true){
+    /*Codigo*/
+}else{
+    /*Codigo*/
+}
+
+/*else-if*/
+if(1 == 1){
+    /*Codigo*/
+} else if(1 != 2){
+    /*Codigo*/
+} else{
+    /*Codigo*/
+}
+
+/*Ternario*/
+const valor = true
+let mensaje = valor == true ? "Es verdadero" : "Es falso"
+
+/*Switch*/
+let numero = 1
+switch (numero){
+    case 1:
+        nombreNumero = "Uno"
+        break
+    case 2:
+        nombreNumero = "Dos"
+        break
+    default:
+        nombreNumero = `Tu numero es ${numero}`
+        break
+}
+
