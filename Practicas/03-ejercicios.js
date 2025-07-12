80cedf0a6e684faf059fe0f70bc79d0283ac1966
Pablo Rojas <123708990+TacticalOnion@users.noreply.github.com>
2025-07-12 07:00:09 +0000

    feat(Practicas): agrega script de ejercicios de variables y tipos de datos

---
+++ Practicas/03-ejercicios.js
@@ -0,0 +1,88 @@
+// 1. Escribe un comentario en una línea
+// Comentario en una linea
+
+// 2. Escribe un comentario en varias líneas
+/*
+Comentario
+en
+varias
+lineas
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let texto = "cadena de texto"
+let entero = 1
+let real = 1.0
+let enteroGrande = BigInt(100000000000000)
+let enteroGrande2 = 10000000000000000000000000n
+let simbolo = Symbol("nombreSimbolo")
+let booleano = true
+let nulo = null
+let indefinido
+
+// 4. Imprime por consola el valor de todas las variables
+/* console.log(texto)
+console.log(entero)
+console.log(enteroGrande)
+console.log(enteroGrande2)
+console.log(real)
+console.log(simbolo)
+console.log(booleano)
+console.log(nulo)
+console.log(indefinido) */
+
+// 5. Imprime por consola el tipo de todas las variables
+/* console.log( typeof texto)
+console.log( typeof entero)
+console.log( typeof real)
+console.log( typeof enteroGrande)
+console.log( typeof enteroGrande2)
+console.log( typeof simbolo)
+console.log( typeof booleano)
+console.log( typeof nulo)
+console.log( typeof indefinido)
+ */
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+/* texto = "Nueva cadena de texto"
+entero = 2
+real = 1.1
+enteroGrande = BigInt(1000000000001)
+enteroGrande2 = 100000000000000000000001n
+simbolo = Symbol("nuevo-nombre")
+nulo = null
+indefinido */
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+/* texto = 2
+entero = "2"
+real = "1.1"
+enteroGrande = "1"
+enteroGrande2 = "2"
+simbolo = "simbolo"
+nulo = "algo"
+indefinido = "algo mas definido" */
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const textoConstante = "cadena de texto"
+const enteroConstante = 1
+const realConstante = 1.0
+const enteroGrandeConstante = BigInt(100000000000000)
+const enteroGrande2Constante = 10000000000000000000000000n
+const simboloConstante = Symbol("nombreSimbolo")
+const booleanoConstante = true
+const nuloConstante = null
+// const indefinidoConstante
+
+
+// 9. A continuación, modifica los valores de las constantes
+/* textoConstante = 2
+enteroConstante = "2"
+realConstante = "1.1"
+enteroGrandeConstante = "1"
+enteroGrande2Constante = "2"
+simboloConstante = "simbolo"
+nuloConstante = "algo"
+ */
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
