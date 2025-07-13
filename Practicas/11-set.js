63693578144d86780efab63615b7e05f842a6d45
Pablo Rojas <123708990+TacticalOnion@users.noreply.github.com>
2025-07-13 00:18:16 +0000

    feat(Practicas): agrega script para probar conjuntos

---
+++ Practicas/11-set.js
@@ -0,0 +1,13 @@
+/*Set*/
+
+let cojunto = new Set(["Palabra",1,true])
+
+/*Metodos*/
+cojunto.add("Palabra2") // Agrega un elemento
+conjunto.delete("Palabra2") // Elimina un elemento especifico
+conjunto.has("Palabra") // Verifica si existe un elemento en el conjunto
+conjunto.size // Tamanio del conjunto
+let arreglo = Array.from(conjunto) // Convierte un conjunto en un arreglo
+let conjunto = new Set(arreglo) // Convertir un arreglo en un conjunto
+
+
